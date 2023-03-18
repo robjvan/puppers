d4b1541f04a3e302cb8992d23b44d3ee9bc0040e
Rob Vandelinder <robjvan@gmail.com>
2023-03-18 00:54:44 -0230

    Added routes for RegisterPage and ForgotPassPage

--- lib/utilities/routes.dart
+++ lib/utilities/routes.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:puppers_mobile/pages/forgot_pass_page/forgot_pass_page.dart';
 import 'package:puppers_mobile/pages/login_page/login_page.dart';
+import 'package:puppers_mobile/pages/register_page/register_page.dart';
 
 class AppRoutes {
   static const String initialRoute = LoginPage.routeName;
@@ -8,6 +10,14 @@ class AppRoutes {
     GetPage<dynamic>(
       name: LoginPage.routeName,
       page: LoginPage.new,
+    ),
+    GetPage<dynamic>(
+      name: RegisterPage.routeName,
+      page: RegisterPage.new,
+    ),
+    GetPage<dynamic>(
+      name: ForgotPassPage.routeName,
+      page: ForgotPassPage.new,
     ),
   ];
 }
